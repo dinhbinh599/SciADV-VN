46e0a0f63178671e368d7a83fd2f3c0302574bee
hoho303 <hoangthuan2092003@gmail.com>
2023-11-09 17:08:52 +0700

    fix ssl

--- AdvWeb_VN/Startup.cs
+++ AdvWeb_VN/Startup.cs
@@ -59,7 +59,7 @@ namespace AdvWeb_VN
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				//app.UseHsts();
 			}
-			//app.UseHttpsRedirection();
+			app.UseHttpsRedirection();
 			app.UseStaticFiles();
 
 			app.UseRouting();
