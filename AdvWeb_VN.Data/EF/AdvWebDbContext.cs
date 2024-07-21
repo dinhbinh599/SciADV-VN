ff1dfdc47346343268101c12d93769f349325d59
Dang Tuan Pham <dinhbinh599@gmail.com>
2024-07-21 21:33:16 +0700

    Update to dotnet 6

--- AdvWeb_VN.Data/EF/AdvWebDbContext.cs
+++ AdvWeb_VN.Data/EF/AdvWebDbContext.cs
@@ -5,48 +5,42 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace AdvWeb_VN.Data.EF
+namespace AdvWeb_VN.Data.EF;
+
+public class AdvWebDbContext : IdentityDbContext<User,Role,Guid>
 {
-	public class AdvWebDbContext : IdentityDbContext<User,Role,Guid>
+	public AdvWebDbContext(DbContextOptions options) : base(options)
 	{
-		public AdvWebDbContext(DbContextOptions options) : base(options)
-		{
-		}
-		protected override void OnModelCreating(ModelBuilder modelBuilder)
-		{
-			//Thiết lập thêm tùy chỉnh cho các bảng
-			modelBuilder.ApplyConfiguration(new CategoryConfiguration());
-			modelBuilder.ApplyConfiguration(new TagConfiguration());
-			modelBuilder.ApplyConfiguration(new PostConfiguration());
-			modelBuilder.ApplyConfiguration(new CommentConfiguration());
-			modelBuilder.ApplyConfiguration(new PostTagConfiguration());
-			modelBuilder.ApplyConfiguration(new PostImageConfiguration());
-			modelBuilder.ApplyConfiguration(new UserConfiguration());
-			modelBuilder.ApplyConfiguration(new RoleConfiguration());
-			modelBuilder.ApplyConfiguration(new SubCategoryConfiguration()); 
-
-
-			modelBuilder.Entity<IdentityUserClaim<Guid>>().ToTable("AppUserClaims");
-			modelBuilder.Entity<IdentityUserRole<Guid>>().ToTable("AppUserRoles").HasKey(x=>new { x.UserId, x.RoleId});
-			modelBuilder.Entity<IdentityUserLogin<Guid>>().ToTable("AppUserLogins").HasKey(x=>x.UserId);
-
-			modelBuilder.Entity<IdentityRoleClaim<Guid>>().ToTable("AppRoleClaims");
-			modelBuilder.Entity<IdentityUserToken<Guid>>().ToTable("AppUserTokens").HasKey(x => x.UserId);
-			
-			modelBuilder.Seed();
-			//base.OnModelCreating(modelBuilder);
-		}
-
-		//Thiết lập DbSet để sau truy vấn đối tượng
-		public DbSet<Post> Posts { set; get; }
-		public DbSet<Category> Categories { set; get; }
-		public DbSet<Comment> Comments { set; get; }
-		public DbSet<Tag> Tags { set; get; }
-		public DbSet<PostTag> PostTags { set; get; }
-		public DbSet<PostImage> PostImages { set; get; }
-		public DbSet<SubCategory> SubCategories { set; get; }
 	}
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		//Thiết lập thêm tùy chỉnh cho các bảng
+		modelBuilder.ApplyConfiguration(new CategoryConfiguration());
+		modelBuilder.ApplyConfiguration(new TagConfiguration());
+		modelBuilder.ApplyConfiguration(new PostConfiguration());
+		modelBuilder.ApplyConfiguration(new CommentConfiguration());
+		modelBuilder.ApplyConfiguration(new PostTagConfiguration());
+		modelBuilder.ApplyConfiguration(new PostImageConfiguration());
+		modelBuilder.ApplyConfiguration(new UserConfiguration());
+		modelBuilder.ApplyConfiguration(new RoleConfiguration());
+		modelBuilder.ApplyConfiguration(new SubCategoryConfiguration()); 
+		modelBuilder.Entity<IdentityUserClaim<Guid>>().ToTable("AppUserClaims");
+		modelBuilder.Entity<IdentityUserRole<Guid>>().ToTable("AppUserRoles").HasKey(x=>new { x.UserId, x.RoleId});
+		modelBuilder.Entity<IdentityUserLogin<Guid>>().ToTable("AppUserLogins").HasKey(x=>x.UserId);
+		modelBuilder.Entity<IdentityRoleClaim<Guid>>().ToTable("AppRoleClaims");
+		modelBuilder.Entity<IdentityUserToken<Guid>>().ToTable("AppUserTokens").HasKey(x => x.UserId);
+		
+		modelBuilder.Seed();
+		//base.OnModelCreating(modelBuilder);
+	}
+	//Thiết lập DbSet để sau truy vấn đối tượng
+	public DbSet<Post> Posts { set; get; }
+	public DbSet<Category> Categories { set; get; }
+	public DbSet<Comment> Comments { set; get; }
+	public DbSet<Tag> Tags { set; get; }
+	public DbSet<PostTag> PostTags { set; get; }
+	public DbSet<PostImage> PostImages { set; get; }
+	public DbSet<SubCategory> SubCategories { set; get; }
 }
+
--- AdvWeb_VN/Program.cs
+++ AdvWeb_VN/Program.cs
@@ -1,26 +1,67 @@
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
+using AdvWeb_VN.Application.Common;
+using AdvWeb_VN.WebApp.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace AdvWeb_VN
+var builder = WebApplication.CreateBuilder(args);
+
+// ConfigureServices method in Startup
+
+builder.Services.AddHttpClient("backendapi", client =>
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			CreateHostBuilder(args).Build().Run();
-		}
+    client.BaseAddress = new Uri(builder.Configuration["BaseAddress"]!);
+});
+builder.Services.AddControllersWithViews();
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				});
-	}
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+builder.Services.AddTransient<IUserApiClient, UserApiClient>();
+builder.Services.AddTransient<IRoleApiClient, RoleApiClient>();
+builder.Services.AddTransient<ICategoryApiClient, CategoryApiClient>();
+builder.Services.AddTransient<IPostApiClient, PostApiClient>();
+builder.Services.AddTransient<ITagApiClient, TagApiClient>();
+builder.Services.AddTransient<ISubCategoryApiClient, SubCategoryApiClient>();
+builder.Services.AddTransient<ICommentApiClient, CommentApiClient>();
+
+IMvcBuilder razorBuilder = builder.Services.AddRazorPages();
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+if (environment == Environments.Development)
+{
+    razorBuilder.AddRazorRuntimeCompilation();
 }
+
+var app = builder.Build();
+
+// Configure method in Startup
+
+// Cập nhật danh sách người donate
+GeneralInformation.Donors = app.Configuration["Donors"]!.Split(",").ToList();
+			
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    //app.UseHsts();
+}
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+
+await app.RunAsync();
